d6cad15756e2a7b80470976cdb975332b200e4f6
crazyironman <crazyscoop@gmail.com>
2018-06-28 11:07:09 +0800

    32

---
+++ files/ebay__/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="ebay__.html",d="generationDate",e=new Date(1530155199853.96),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="SelectTab",n="page",o="packageId",p="f7a9fbbd8b0742e2bb3cf8d5ffc48171",q="type",r="Axure:Page",s="name",t="ebay刊登",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="b69a126cf06b41b289b77ff1b01f1b0d",Y="label",Z="friendlyType",ba="图像",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=1219,bj="height",bk=645,bl="imageOverrides",bm="images",bn="normal~",bo="images/ebay__/u983.png",bp="masters",bq="objectPaths",br="b69a126cf06b41b289b77ff1b01f1b0d",bs="scriptId",bt="u983";
+return _creator();
+})());
